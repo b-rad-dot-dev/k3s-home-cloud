c18241af943074c89f8c7605976eff9010a67dfe
Bradley Mize <general_bradley@hotmail.com>
2026-04-18 21:54:19 -0700

    - Added script to install kubectl

--- cluster/webtop/docker/install_scripts/install_kubectl.sh
+++ cluster/webtop/docker/install_scripts/install_kubectl.sh
@@ -8,6 +8,6 @@ echo "------------------------------------------------"
 # -x print command before executing it
 set -ex
 
-curl -LO https://dl.k8s.io/release/v1.32.0/bin/linux/amd64/kubectl
+curl -LO https://dl.k8s.io/release/v1.35.0/bin/linux/amd64/kubectl
 chmod +x kubectl
 mv kubectl /usr/local/bin/
