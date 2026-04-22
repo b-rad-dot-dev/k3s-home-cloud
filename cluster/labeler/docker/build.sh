f0ff5cf5b0a95543e7f4666d80a0f4dd1625d53e
Bradley Mize <bradley.mize@gmail.com>
2026-04-21 19:28:03 -0700

    - Built new version of image w/o mounted source code - Pushed to registry - Updated chart values

--- cluster/labeler/docker/build.sh
+++ cluster/labeler/docker/build.sh
@@ -10,6 +10,3 @@ docker build \
   -t "registry.b-rad.dev:443/labeler:latest" \
   -t "registry.b-rad.dev:443/labeler:${timestamp}" \
   .
-
-
-docker run --rm --privileged -v /dev/bus/usb:/dev/bus/usb -p 5000:5000 registry.b-rad.dev:443/labeler:latest
--- cluster/labeler/docker/push.sh
+++ cluster/labeler/docker/push.sh
@@ -2,7 +2,5 @@
 
 timestamp="$(cat version)"
 
-echo "${timestamp}" > version
-
 docker push "registry.b-rad.dev:443/labeler:latest"
 docker push "registry.b-rad.dev:443/labeler:${timestamp}"
