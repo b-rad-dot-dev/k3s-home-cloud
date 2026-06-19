3ec85e590c7867548da3e85b54482b14d2715c3e
B-rad <brad@b-rad.dev>
2026-06-19 14:30:51 -0700

    - Migrated labeler to new helm chart - Deleted obsolete labeler + md-to-pdf local charts

--- cluster/md-to-pdf/push.ps1
+++
@@ -1,6 +0,0 @@
-﻿# Push script for md-to-pdf Docker image
-
-$timestamp = Get-Content ".\version" -Raw
-
-docker push "registry.b-rad.dev:443/md-to-pdf:latest"
-docker push "registry.b-rad.dev:443/md-to-pdf:${timestamp}"
--- cluster/md-to-pdf/push.sh
+++
@@ -1,6 +0,0 @@
-﻿#!/usr/bin/env bash
-
-timestamp="$(cat version)"
-
-docker push "registry.b-rad.dev:443/md-to-pdf:latest"
-docker push "registry.b-rad.dev:443/md-to-pdf:${timestamp}"
