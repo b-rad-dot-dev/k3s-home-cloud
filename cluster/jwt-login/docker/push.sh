932d773ce5966d5c3dfe5d1800ea8f8fbd625b6a
B-rad <brad@b-rad.dev>
2026-06-25 19:28:09 -0700

    - Replaced embedded jwt-login chart with one from repo

--- cluster/jwt-login/docker/push.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-timestamp="$(cat version)"
-
-echo "${timestamp}" > version
-
-docker push "registry.b-rad.dev:443/jwt-login:latest"
-docker push "registry.b-rad.dev:443/jwt-login:${timestamp}"
--- cluster/jwt-login/start.bat
+++
@@ -1,1 +0,0 @@
-docker run --rm -it --net=host -v "%CD%:/wd" --workdir /wd postgres:17 /bin/bash
